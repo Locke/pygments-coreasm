d2b80b8002afbee3d469af31536da7145bfe9e41
André Wolski <andre@dena-design.de>
2016-09-16 22:18:15 +0200

    more keywords

--- coreasm.py
+++ coreasm.py
@@ -41,22 +41,30 @@ class CoreASMLexer(RegexLexer):
         ],
         'root': [
             include('commentsandwhitespace'),
-            (r'->', Punctuation),
-            (r'([-<>+*/]|=|!=|<=|>=)', Operator),
-            (r'(not|or|and)', Operator.Word),
-            (r'[{(\[:;,]', Punctuation),
+            (r'(->|:=|:)', Punctuation),
+            (r'(memberof|implies|not|xor|or|and|div)', Operator.Word),
+            (r'[{(\[,|?]', Punctuation),
             (r'[})\].]', Punctuation),
-            (r'(for|in|while|do|return|switch|case|if|then|else)\b', Keyword),
+            (r'(<<|>>)', Punctuation),
+            (r'([-<>+*/%]|=|!=|<=|>=)', Operator),
+            (r'(end)?(seqblock|par|choose|case)\b', Keyword),
+            (r'(seq|next|iterate|return|result)', Keyword),
+            (r'(import|extend|do|skip|pick|forall|exists|in|is|with|holds|ifnone|if|then|else|while|step)\b', Keyword),
+            (r'(add|remove|push|pop|shift (left|right)|(en|de)queue|from|(in)?to)', Keyword),
+            (r'(deuginfo)', Keyword),
+            (r'(suspend|resume|terminate|shutdown)', Keyword),
             (r'(include)(\s+)', bygroups(Keyword, Text), 'string'),
             (r'(CoreASM|use)(\s+)(\w+)', bygroups(Keyword.Namespace, Text, Name.Namespace)),
             (r'(init)(\s+)(\w+)', bygroups(Keyword.Namespace, Text, Name.Function)),
+            (r'(call)(\s+)(\w+)', bygroups(Keyword, Text, Name.Function)),
             (r'(option)(\s+)([\w.]+)(\s+)(\w+)', bygroups(Keyword.Namespace, Text, Name, Text, Name)),
-            (r'(let|universe)\b', Keyword.Declaration),
+            (r'(@)(\w+)', bygroups(Operator, Name.Function)),
+            (r'(ruleelement)(\s+)(\w+)', bygroups(Operator.Word, Text, Name.Function)),
+            (r'(local|let|universe|enum)\b', Keyword.Declaration),
             (r'(function|derived|rule)(\s+)(\w+)', bygroups(Keyword.Declaration, Text, Name.Function)),
-            #(r'(boolean|number)\b', Keyword.Reserved),
-            (r'(true|false|undef)\b', Keyword.Constant),
+            (r'(true|false|undef|self|program)\b', Keyword.Constant),
             (r'(STRING|NUMBER|MAP|SET|RULE)\b', Name.Builtin),
-            (r'[0-9][0-9]*\.[0-9]', Number.Float),
+            (r'(input)\b', Name.Builtin),
             (r'[0-9]+', Number.Integer),
             include('string'),
         ]
@@ -83,19 +91,39 @@ function sBPMFunction : NUMBER * STRING -> RULE
 rule Foo(x) = {
   while (i < 5 + (2*3/4)) do {
     if (x = true and y != undef) then {
+      shift right value into loc
       //not good
       /*
       at least not undef
       */
       Crash()
     }
-    else { print "asdf!" }
-  }
-  
-  let x = foo(y) in {
-    print "x = " + x
+    else { print "asdf: " + (1 + 12.37 + |{"a", {4 -> undef}}|) }
   }
 }
+ 
+derived spameggs = return fish in {
+  let x = foo(y) in {
+    print "x = " + x
+    fish := << spam | spam in eggs with |spam| < x >>
+  }
+}
+
+rule InitRule =
+  par
+    terminate := false
+    program(self) := @MainProgram
+    program(foo) := ruleelement MainProgram
+  endpar
+
+rule MainProgram =
+  if not terminate then
+    par
+      print "This is CoreASM."
+      terminate := true
+    endpar
+  else
+    program(self) := undef
 """
 
     print highlight(code, CoreASMLexer(), TerminalFormatter())
